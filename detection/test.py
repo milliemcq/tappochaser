3cdc8046a965af85e82b650d51a6da666588cccd
Nikhil Mohan <nikil@Nikhils-MacBook-Pro.local>
2022-07-20 11:16:23 +0100

    clean

---
+++ detection/test.py
@@ -0,0 +1,10 @@
+from PIL import Image
+from PIL import ImageOps
+from detect import detect
+from utils import voc_labels
+img_path = 'dog.jpg'
+original_image = Image.open(img_path, mode='r')
+original_image = original_image.convert('RGB')
+suppress_labels = [label for label in voc_labels if label not in ["dog"]]
+detect(original_image, min_score=0.2, max_overlap=0.1, top_k=1000, suppress=suppress_labels).show()
+print("test")
